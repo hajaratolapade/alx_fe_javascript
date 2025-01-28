93edf63523bb287595661d609615164dc5310fe0
hajaratolapade <olapadehajarat@outlook.com>
2025-01-28 10:57:45 +0100

    create add quote form

---
+++ script.js
@@ -0,0 +1,5 @@
+<div>
+    <input id="showRandomQuote" type="text" placeholder="Love is a beautiful thing" />
+    <input id="Love" type="text" placeholder="Heart Matters" />
+    <button onclick="I love love()">Add Quote</button>
+  </div>
